707aa83479601c0ac70a1d059af0f282156e44ff
SaiSankar <gunapu.saisankar11@gmail.com>
2026-03-09 23:38:50 +0530

    Assignment 1

---
+++ typescript-training/assignment1.ts
@@ -0,0 +1,98 @@
+//Temperature of a city in degrees Celsius: 25.5
+let Temp:number =25.5;
+console.log("Temp: ",Temp);
+
+//Whether a customer has placed an order: true or false
+let orderPlaced :boolean=true;
+console.log("orderPlaced: ",orderPlaced);
+
+//Person's phone number: "123-456-7890"
+let phoneNumber:number=123-456-7890;
+console.log("phoneNumber: ",phoneNumber);
+
+//Amount of money in a customer's bank account: 1000.50
+let balance:number=1000.50;
+console.log("balance: ",balance);
+
+//Person's email address: "john.doe@example.com"
+let emailaddress:string="john.doe@example.com";
+console.log("emailaddress: ",emailaddress);
+
+//Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+let  latitude:number= 37.7749;
+let longitude:number=-122.4194;
+//using tuple
+let Coordinates:[number,number]=[37.7749, -122.4194];
+console.log("Coordinates: ",Coordinates);
+
+//Person's marital status: true or false
+let martialStatus:boolean=false;
+console.log("martialStatus: ",martialStatus);
+
+//Person's occupation: "Software Engineer"
+let occupation:string="Software Engineer";
+console.log("occupation: ",occupation);
+
+//Person's favourite colour: "Blue"
+let favouriteColour:string="Blue";
+console.log("favouriteColour: ",favouriteColour);
+
+//Current year: 2023
+let currentYear:number=2023;
+console.log("currentYear: ",currentYear);
+
+//Number of followers on a social media platform: 1,000,000
+let numFollowers:number=1000000;
+console.log("numFollowers: ",numFollowers);
+
+//Rating of a movie
+let rateMovie:number=7.5;
+console.log("rateMovie: ",rateMovie);
+
+//Person's blood type: 'A'
+let bloodType:string="A";
+console.log("bloodType: ",bloodType);
+
+
+//Title of a book:
+let bookTitle:string="To Kill a Mockingbird";
+console.log("bookTitle: ",bookTitle);
+
+
+//Number of employees in a company
+let numOfemployees:number=500;
+console.log("numOfemployees: ",numOfemployees);
+
+//Time of an event
+let time:string="2:30 PM";
+console.log("time: ",time);
+
+//Name of a country
+let country:string="United States";
+console.log("country: ",country);
+
+//Person's eye color
+let personEyecolour:string="Brown";
+console.log("personEyecolour: ",personEyecolour);
+
+//Person's birthplace
+let birthplace:string="New York City";
+console.log("birthplace: ",birthplace);
+
+//Distance between two cities
+let distance:number=200.5;
+console.log("distance: ",distance);
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
